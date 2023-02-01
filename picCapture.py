6a23728f600a109d5d021b8bfebc5b5344ef9e3d
vidur2 <vmod2005@gmail.com>
2023-02-01 18:25:44 -0500

    pose

---
+++ picCapture.py
@@ -0,0 +1,22 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+width=1000
+counter = 0
+
+while (True):
+    ret, frame = cam.read()
+
+    if (ret):
+        cv2.imshow("view", frame)
+        h, w, _ = frame.shape
+
+        key = cv2.waitKey(1)
+        height = int(width*(h/w))
+        frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_CUBIC)
+
+        if ord("c") == key:
+            cv2.imwrite(f"./targets/img{counter}.jpeg", frame)
+            counter += 1
+        elif ord("q") == key:
+            break
